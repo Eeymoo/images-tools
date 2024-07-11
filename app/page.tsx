0c5469c25aac209e71b23000776ce803ea3fc127
Eeymoo <eeymoo.xmail.fun>
2024-07-11 23:03:57 +0800

    UI 构建

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
 
 export default function Home() {
+  const [config, setConfig] = useState({
+    quality: 40
+  });
+  const [images, setImages] = useState<File[]>([]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col relative overflow-x-auto h-screen">
+      {/* 导航 */}
+      <div className="navbar bg-base-100 shadow-xl">
+        <div className="container mx-auto">
+          <div className="flex-1">
+            <a href="/" className="btn btn-ghost text-xl">daisyUI</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li><a href="https://github.com/Eeymoo/images-tools">Github</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="container grow card glass card-side bg-base-100 shadow-xl flex p-16 mx-auto my-8">
+        {/* 图片操作区 */}
+        <div className="grow h-full w-auto mr-8">
+          <div className="flex flex-row flex-wrap mx-auto justify-around">
+            {
+              images.map(image => {
+                return (
+                  <div
+                    key={image.name}
+                    id={image.name}
+                    className="flex bg-slate-100 size-48 rounded-lg m-3 bg-cover p-2 flex-wrap"
+                    style={{
+                      backgroundImage: `url(${image.url})`,
+                    }}
+                  >
+                    <input type="checkbox" checked className="checkbox" />
+                    <button className="btn btn-primary btn-xs self-end">删除</button>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        {/* 参数调整区 */}
+        <div className="card bg-white flex flex-col justify-center items-center w-xs p-8">
+          <label className="form-control w-full max-w-xs mb-2">
+            <input type="file" multiple className="file-input file-input-bordered w-full max-w-xs" onChange={e => setImages([...images, ...e.target.files])}/>
+            <div className="label">
+              <span className="label-text-alt">可以上传一个或者多个文件</span>
+            </div>
+          </label>
+          <label className="form-control w-full max-w-xs mb-2">
+            <input type="range" min={0} max="100" value={config.quality} onChange={e => setConfig({ ...config, quality: e.target.value })} className="range" />
+            <div className="label">
+              <span className="label-text-alt">压缩百分比</span>
+              <span className="label-text-alt input input-ghost input-xs -mr-1 w-24">
+                <input type="text" placeholder="压缩百分比" value={config.quality} onChange={e => setConfig({ ...config, quality: e.target.value })} className="w-5/6 text-right" />
+                <span className="w-1/6">%</span>
+              </span>
+            </div>
+          </label>
+          <label className="form-control w-full max-w-xs mb-2">
+            <label className="input input-bordered flex items-center grow">
+                W
+                <input type="text" className="grow mx-2" placeholder="Wight" />
+                px
+              </label>
+          </label>
+          <label className="form-control w-full max-w-xs mb-2">
+            <label className="input join-item input-bordered flex items-center">
+              H
+              <input type="text" className="grow mx-2" placeholder="Height" />
+              px
+            </label>
+          </label>
+          <label className="form-control w-full max-w-xs mb-2">
+            <label className="input join-item input-bordered flex items-center">
+              Size
+              <input type="text" className="grow mx-2" placeholder="Height" />
+              KB
+            </label>
+            <div className="label">
+              <span className="label-text-alt">与实际存在差异但始终小于此值</span>
+            </div>
+          </label>
+          <select className="select select-bordered w-full max-w-xs mb-4">
+            <option disabled selected>原始格式</option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select>
+          <button className="btn w-full btn-neutral">压缩</button>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* 页脚 */}
+      <footer className="footer footer-center bg-base-300 text-base-content p-4">
+        <aside>
+          <p>Copyright © ${new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+        </aside>
+      </footer>
     </main>
   );
 }
